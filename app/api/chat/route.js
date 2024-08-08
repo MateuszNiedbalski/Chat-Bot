a4316d6b0966d611039f7a6612feed68402ef8fe
Damy Matuwidi <dmatuwidi@gmail.com>
2024-08-08 04:09:17 +0100

    Fixed backend to use GenAI and fixed frontend to create working app

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,28 +1,69 @@
-import {NextResponse} from 'next/server' // Import NextResponse from Next.js for handling responses
-const {VertexAI, HarmCategory, HarmBlockThreshold} = require('@google-cloud/vertexai');
-import { useState, useEffect } from "react";
+import { NextResponse } from 'next/server' // Import NextResponse from Next.js for handling responses
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const project = 'extreme-cycling-430919-i7';
-const location = 'us-central1';
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const [chat, setChat] = useState();
+  // Gemini model
+  const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
 
-const systemPrompt = "You are an AI chatbot trying to support people for Headstarter. Headstarter is a company that runs programmes from people to learn AI skills through project based learning, AI interviews and teaching people networking skills"
+// (Deprecated) const systemPrompt = You are an AI chatbot trying to support people for Headstarter. Headstarter is a company that runs programmes from people to learn AI skills through project based learning, AI interviews and teaching people networking skills";
 
-export async function ChatBegin() {
-  const vertex_ai = new VertexAI({project: project, location: location});
+export async function POST(req) {
+  const data = await req.json();
 
-  const generativeModel = vertex_ai.getGenerativeModel({
-    model: 'gemini-pro',
-    safety_settings: [{category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE}],
-    generation_config: {max_output_tokens: 2560},
+  // Instantiate chat request to the API using previous chat history
+  const chat = model.startChat({
+    history: [
+      ...data.slice(0, data.length - 1), // Omit user input from current history
+    ],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
   });
 
+  // (DEBUG) POST Request body
+  console.log("DATA:" + JSON.stringify(data));
+  // (DEBUG) UserInput
+  console.log("INPUT:" + JSON.stringify(data[data.length-1].parts[0].text));
 
-  setChat(generativeModel.startChat());
-}
+  // Sends user input to the chat model
+  const result = await chat.sendMessageStream(data[data.length-1].parts[0].text);
 
-export async function POST(req){
-  const result1 = await chat.sendMessage(req);
-  return JSON.stringify(await result1.response)
+  /* (Deprecated)
+  let text = "";
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    console.log(chunkText);
+    text += chunkText;
+  };
+  */
+
+  // New ReadableStream to handle stream response
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+
+      try {
+        // Iterate over the streamed chunks of response
+        for await (const chunk of result.stream) {
+          const content = chunk.text();
+
+          if (content) {
+            console.log(content);
+            controller.enqueue(encoder.encode(content));
+          }
+        }
+
+      } catch (err) {
+        // Handle errors occured during streaming
+        controller.error(err);
+      } finally {
+        // Close the stream when done
+        controller.close();
+      }
+    }
+  })
+
+  // Return the stream response
+  return new NextResponse(stream);
 }
