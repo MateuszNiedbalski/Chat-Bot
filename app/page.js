738f2c472e1f4bf3f3c86fe5adf65e8f7d5bee21
Shayan I Janjua <shayanjanjua.london@gmail.com>
2024-08-09 00:59:53 +0100

    UI changes

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 'use client'
 
-import { Box, Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 import { useState, useRef, useEffect } from 'react';
 import Markdown from 'react-markdown';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import Rating from '@mui/material/Rating';
+
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -12,6 +18,46 @@ export default function Home() {
     }
   ]);
 
+  //for green online indicator for profile image
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      backgroundColor: '#44b700',
+      color: '#44b700',
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: 'ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    '@keyframes ripple': {
+      '0%': {
+        transform: 'scale(.8)',
+        opacity: 1,
+      },
+      '100%': {
+        transform: 'scale(2.4)',
+        opacity: 0,
+      },
+    },
+  }));
+
+  //for better looking star ratings
+  const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+      color: '#45A29E',
+    },
+    '& .MuiRating-iconHover': {
+      color: '#45A29E',
+    },
+  });
+  
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false)
   // Send message function
@@ -104,23 +150,36 @@ export default function Home() {
 
   // UI
   return (
-    <Box width="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-      <Stack direction={'column'} width="500px" height="700px" border="1px solid black" p={2} spacing={3}>
+    <Box width="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor = '#0B0C10'>
+      <Stack direction={'column'} width="500px" height="700px" border="1px"  p={2} spacing={3} sx={{ borderRadius: '16px',borderColor:'#66FCF1' }}>
+      <Stack direction = {'row'} width="467px" height="70px" bgcolor = "#45A29E"  spacing={1} alignItems={'center'} margin ={0} padding = {2}  sx={{ borderRadius: '16px' }}>
+        <Box width = "50px" height = "46px"><StyledBadge overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} variant="dot">
+        <Avatar src="/modelavatar.jpg" />
+      </StyledBadge>
+      </Box>
+      <Typography color='#0b0c10' variant ='h5' >Headstarter Support Assistant</Typography>
+      </Stack>
         <Stack direction={'column'} spacing={2} flexGrow={1} overflow="auto" maxHeight="100%">
           {messages.map((message, index) => (
             <Box key={index} display="flex" justifyContent={message.role === 'model' ? 'flex-start' : 'flex-end'}>
-              <Box bgcolor={ message.role === 'model' ? 'primary.main' : 'secondary.main'} color="white" borderRadius={1} p={3}>
+              <Stack direction={message.role === 'model' ? 'row' : 'row-reverse'}>
+              <Avatar src={message.role==='model' ? '/modelavatar.jpg' : '/user.png'} />
+              <Box bgcolor={ message.role === 'model' ? '#9ef7f1' : '#C5C6C7'} color="#1F2833" borderRadius={1} p={3}>
                 <Markdown>{message.parts[0].text}</Markdown>
+                {message.role === 'model' && (
+                    <Box display="flex" alignItems="center" mt={1}>
+                      <StyledRating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </Box>
+                  )}
               </Box>
+              </Stack>
             </Box>
           ))}
           <div ref={messagesEndRef} />
         </Stack>
         <Stack direction={'row'} spacing={2}>
-          <TextField label="Message" fullWidth value={message} placeholder='Say "Hello" to begin chatting!' onChange={(e) => setMessage(e.target.value)} onKeyPress={handleKeyPress} disabled={isLoading}></TextField>
-          <Button variant="contained" onClick={sendMessage} disabled={isLoading}>
-            Send
-          </Button>
+          <TextField label="Message" fullWidth value={message} placeholder='Say "Hello" to begin chatting!' onChange={(e) => setMessage(e.target.value)} onKeyPress={handleKeyPress} disabled={isLoading} sx = {{'& .MuiInputBase-input': { color: '#C5C6C7' }, '& .MuiInputLabel-root': { color: '#C5C6C7' }, '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': { borderColor: '#C5C6C7' }, '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: '#00d1c4', color: '#00d1c4' },'& .MuiInputLabel-root': { color: '#C5C6C7',},'& .MuiInputLabel-root.Mui-focused': {color: '#00d1c4',}}}></TextField>
+          <Button variant="contained" onClick={sendMessage} disabled={isLoading} sx = {{bgcolor: '#00635d', color: '#C5C6C7', '&:hover': { backgroundColor: '#00d1c4',}}} ><SendIcon  /></Button>
         </Stack>
       </Stack>
     </Box>
